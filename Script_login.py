fbc74cc8ea41987e02e83c9383e43e68e0d053a6
Agilbagus B.A <70071422+AgilbagusBA@users.noreply.github.com>
2020-09-23 21:47:38 +0700

    Add files via upload

---
+++ Script_login.py
@@ -0,0 +1,14 @@
+print ("\n")
+print ("+---------------------+")
+print ("|\tPROGRAM LOGIN\t|")
+print ("+---------------------+")
+print ("\n")
+nama = input("Username\t: ")
+sandi = input("Password\t: ")
+if nama == "agilbagus" and sandi == "root":
+    print("\n\tLogin Sukses\n")
+else:
+    print("Username atau Password salah!")
+
+#username bisa di ganti terserah
+#password harus root
